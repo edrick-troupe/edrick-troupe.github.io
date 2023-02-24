75edda0d00f7ecb7939b8e90529bc940221df66a
Edrick101 <alphonse.61000@gmail.com>
2023-02-24 13:59:05 +0100

    Annulation de la traduction avec "mouseout"

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,19 @@
 // Text transformation
 
 const introText = document.querySelector(".intro_text");
-let newText = '<a href="https://github.com/edrick-troupe">You wanna know a secret ?</a>'
+let englishText = '<a href="https://github.com/edrick-troupe">You wanna know a secret ?</a>'
+let greekText = '<a href="https://github.com/edrick-troupe">Θέλεις να μάθεις ένα μυστικό ?</a>'
 
-introText.addEventListener("mouseover", translate);
+introText.addEventListener("mouseover", englishVersion);
 
-function translate() {
+function englishVersion() {
     introText.innerHTML = "";
-    introText.innerHTML += newText;
+    introText.innerHTML += englishText;
+}
+
+introText.addEventListener("mouseout", greekVersion);
+
+function greekVersion() {
+    introText.innerHTML = "";
+    introText.innerHTML += greekText;
 }
